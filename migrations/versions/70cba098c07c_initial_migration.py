80fbc7768a893636ed0211c00cf039f52c7dce67
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2020-11-02 20:15:05 -0300

    migration

--- migrations/versions/70cba098c07c_initial_migration.py
+++ migrations/versions/70cba098c07c_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration.
 
-Revision ID: bf5990b6a65a
+Revision ID: 70cba098c07c
 Revises: 
-Create Date: 2020-10-20 17:33:57.075913
+Create Date: 2020-11-02 20:12:33.628322
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'bf5990b6a65a'
+revision = '70cba098c07c'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -27,17 +27,22 @@ def upgrade():
     sa.Column('status', sa.Enum('available', 'in_negotiation', 'paid', 'delivered', 'finished', name='announcementstatusenum'), nullable=True),
     sa.Column('type_', sa.Enum('item', 'account', 'gold', name='announcementtypeenum'), nullable=True),
     sa.Column('salesman_uuid', sa.Integer(), nullable=True),
+    sa.Column('game', sa.String(length=255), nullable=True),
+    sa.Column('server', sa.String(length=255), nullable=True),
+    sa.Column('created_at', sa.DateTime(), nullable=True),
+    sa.Column('updated_at', sa.DateTime(), nullable=True),
     sa.PrimaryKeyConstraint('uuid')
     )
     op.create_table('users',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=255), nullable=False),
+    sa.Column('nickname', sa.String(length=255), nullable=False),
     sa.Column('phone', sa.String(length=255), nullable=False),
     sa.Column('email', sa.String(length=255), nullable=False),
     sa.Column('password', sa.String(length=255), nullable=False),
     sa.Column('cpf', sa.String(length=255), nullable=False),
     sa.Column('is_salesman', sa.Boolean(), nullable=False),
-    sa.Column('avatar', sa.String(length=255)),
+    sa.Column('avatar', sa.String(length=255), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('cpf'),
     sa.UniqueConstraint('email'),
