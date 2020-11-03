b3ce7ef73f87e1c26e0393059dc13c630f130c15
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2020-11-03 20:36:07 -0300

    readme

--- roct/views/users.py
+++ roct/views/users.py
@@ -3,9 +3,11 @@ from roct import db
 from roct.models import User
 from flask_jwt_extended import jwt_required
 from roct.utils import check_user_is_same
+from flask_bcrypt import Bcrypt
+
 
 users_resource = Blueprint('users', __name__)
-
+bcrypt = Bcrypt()
 
 @users_resource.route('', methods=['POST'])
 def create():
@@ -23,6 +25,9 @@ def create():
 @check_user_is_same
 def update(id):
     data = request.get_json()
+
+    del data['password']
+
     user = User.query.get_or_404(id)
 
     user.query.update(data)
@@ -36,5 +41,20 @@ def update(id):
 @check_user_is_same
 def get(id):
     user = User.query.get_or_404(id)
+
+    return user.serialize()
+
+
+@users_resource.route('<id>/changePassword', methods=['PUT'])
+@jwt_required
+@check_user_is_same
+def change_password(id):
+    data = request.get_json()
+    user = User.query.get_or_404(id)
+
+    data['password'] = bcrypt.generate_password_hash(data['password']).decode()
+
+    user.query.update(data)
+    db.session.commit()
 
     return user.serialize()
