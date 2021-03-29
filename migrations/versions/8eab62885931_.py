903cfd1a940a327dc65a56a4893781803790025f
Hemillainy Santos <hemillainy.santos@ccc.ufcg.edu.br>
2021-03-28 22:17:43 -0300

    update migrations

---
+++ migrations/versions/8eab62885931_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8eab62885931
+Revises: 4f4cc74aa77d
+Create Date: 2021-03-28 22:15:24.036167
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8eab62885931'
+down_revision = '4f4cc74aa77d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
